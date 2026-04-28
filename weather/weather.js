7ad0556e9dbd5dc12b1d54e14960e1903755d515
brookedimaggio204-ops <brookedimaggio204@gmail.com>
2026-04-27 22:41:17 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -16,6 +16,12 @@ $.getJSON(weatherAlertsUrl, function(data) {
   if (feature.properties.severity === 'Severe') alertColor = 'red';
   return { color: alertColor };
 },
+  if (feature.properties.severity === 'Extreme') alertColor = 'lightsalmon';
+  return { color: alertColor };
+},
+  if (feature.properties.severity === 'Minor') alertColor = 'red';
+  return { color: alertColor };
+},
   onEachFeature: function(feature, layer) {
   layer.bindPopup(feature.properties.description);
 }
